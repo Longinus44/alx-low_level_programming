73bc65a217762f3a878c2a5c5b038cc6dc9d3597
Longinus44 <chijiokelonginus4@gmail.com>
2023-07-18 23:47:45 -0700

    completed add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function adds two integer, 'i' and 'j'
+ * @i:first number to add
+ * @j: second number to add
+ *
+ * Return: 0.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
