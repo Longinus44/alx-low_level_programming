73f7194679f5aa89cfedb885d72235d10f6adfbb
Longinus44 <chijiokelonginus4@gmail.com>
2023-07-18 21:49:33 -0700

    project absolute value done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function that computes absolute value
+ * @i: number to check
+ *
+ * Return: 0 (Success)
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
