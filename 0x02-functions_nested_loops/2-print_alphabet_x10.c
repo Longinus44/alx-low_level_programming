a6acda4967cf60d6a915cbb82cfc167b35200e81
Longinus44 <chijiokelonginus4@gmail.com>
2023-07-18 20:39:51 -0700

    task to print alphabet x10 completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints x10 alphabets in lowercase case
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
