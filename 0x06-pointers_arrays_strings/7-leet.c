9c0a43e6e8d4615b990f3e0a53e37c3be4e65c0d
Longinus44 <chijiokelonginus4@gmail.com>
2023-07-27 05:59:07 -0700

    settled the error

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,22 +6,17 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	i = 0;
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
-		i++;
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+				str[i] = s2[j];
+		}
 	}
 	return (str);
 }
