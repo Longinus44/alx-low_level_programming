5fc66df1a3127beaafe6fab608e010a1daf1cacc
Longinus44 <chijiokelonginus4@gmail.com>
2023-07-27 01:47:57 -0700

    added directory for more C-pointers and array strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
