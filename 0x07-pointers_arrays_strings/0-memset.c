b8bae78bf3458975f52b13eeb4765416d07c0651
Longinus44 <chijiokelonginus4@gmail.com>
2023-07-31 16:54:10 -0700

    added a repo for more C pinters and arrays

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memset - function that fills memory with constant byte
+ * @s: memory area
+ * @b:constant byte
+ * @n: bytes of the memory area pointed to by
+ *
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
