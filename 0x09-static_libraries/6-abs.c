fb572dae0fe5fcbf0458571bf27b253107877c29
Longinus <chijiokelonginus4@gmail.com>
2023-08-05 03:52:07 +0100

    done

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute integer value
+ * @n: the int
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
