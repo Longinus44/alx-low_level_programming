e41a091d175dbdcdfb29321246b36ed63482cfb5
longinus4 <chijiokelonginus4@gmail.com>
2024-01-04 00:08:22 +0100

    done

--- 0x0C-more_malloc_free/convert_newlines.sh
+++ 0x0C-more_malloc_free/convert_newlines.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 # Loop through all .c files in the current directory
-for file in *.py; do
+for file in *.c; do
     dos2unix "$file"
 done
 
 echo "Newline conversion complete."
+
+# Loop through all .py files in the current directory
+# for file in *.py; do
+#     if [ -e "$file" ] && [ -f "$file" ]; then
+#         dos2unix "$file"
+#     else
+#         echo "Skipping $file, not a regular file."
+#     fi
+# done
+
+# echo "Newline conversion complete."
